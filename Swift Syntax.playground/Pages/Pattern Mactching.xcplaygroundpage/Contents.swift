c37f2d0f3264fa0eed4a0a835d57d574441ffa59
iyinraphael <iyinraphael@gmail.com>
2020-04-20 19:39:02 -0700

    pattern matching with tuples

--- Swift Syntax.playground/Pages/Pattern Mactching.xcplaygroundpage/Contents.swift
+++ Swift Syntax.playground/Pages/Pattern Mactching.xcplaygroundpage/Contents.swift
@@ -1,7 +1,77 @@
-//: [Previous](@previous)
+//MARK: - Basic Switch
 
-import Foundation
+//Working with one value
+let name = "twostraws"
 
-var str = "Hello, playground"
+switch name {
+case "bilbo":
+    print("Hello, Bilbo Baggins")
+case "twostraws":
+    print("Hello, Paul Hudson")
+default:
+    print("Authentication failed!")
+}
 
-//: [Next](@next)
+//Working with two or more values
+let password = "fr0st1es"
+
+switch (name, password) {
+case ("bilbo", "bagg1n5"):
+    print("Hello, Bilbo Baggins")
+case ("twostraws", "fr0st1es"):
+    print("Hello, Paul Hudson")
+default:
+    print("Who are you?")
+}
+
+//Combining two values into a single tuple
+//In this instance, both parts of the tupple must match the case in order for it to executed
+let authentication = (name: "twostraws", password: "fr0st1es")
+
+switch authentication {
+case ("bilbo", "bagg1n5"):
+    print("Hello, Bilbo Baggins")
+case ("twostraws", "fr0st1es"):
+    print("Hello, Paul Hudson")
+default:
+    print("Who are you?")
+}
+
+//When working with tuples, there are some occasions when you want a partial match
+let authenticationIP = (name: "jabread", password: "UrbanPlan", ipAddress: "127.0.0.1")
+
+switch authenticationIP {
+case ("makin", "Pythonic", _):
+    print("Hello, Makin Ajileye")
+case ("jabread", "UrbanPlan", "127.0.0.1"):
+    print("Hello, iyin Raphael")
+default:
+    print("Who you be?")
+}
+
+//Matchinng with part of tuples and still want to know what the other part is
+switch authenticationIP {
+case ("jabread", let passcode, "127.0.0.1"):
+    print("Hello iyin Raphael, your password is \(passcode)")
+default:
+    print("Who are you?")
+}
+
+//Using calculated tuples to solve FizzBuzz code challenge instead of static tuples
+func fizzbuzz(_ number: Int) -> String {
+    switch (number % 3 == 0, number % 5 == 0) {
+    case (true, false):
+        return "Fizz"
+    case (false, true):
+        return "Buzz"
+    case (true, true):
+        return "FizzBuzz"
+    case (false, false):
+        return String(number)
+    }
+}
+print(fizzbuzz(15))
+
+
+
+//MArk=
